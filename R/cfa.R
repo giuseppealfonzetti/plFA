553d97f8a9aeae56a038f32d8013004a589fa4f7
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2025-01-14 20:23:12 +0800

    Add cfa function

---
+++ R/cfa.R
@@ -0,0 +1,90 @@
+is_ordinal_df <- function(D) {
+  if (!is.data.frame(D)) return(FALSE)
+  else all(sapply(D, is.ordered))
+}
+
+cfa <- function(
+    model,
+    data,
+    start = NULL,
+    estimator = "SPML",
+    estimator.args = list(
+      method = "hyper",
+      valdata = NULL,
+      iterations_subset = NULL,
+      ncores = 1
+    ),
+    information = "observed",
+    std.lv = TRUE,
+    control = list(),
+    ...
+) {
+
+  lavargs <- list(...)
+
+  # Validate arguments
+  if (!is_ordinal_df(data)) {
+    cli::cli_abort("Ordinal data required")
+  }
+  if (isFALSE(std.lv)) {
+    cli::cli_alert_warning("Non-standardised latent variable estimation not implemented yet. Setting `std.lv = TRUE`.")
+    std.lv <- TRUE
+  }
+  lavargs$std.lv <- std.lv
+  lavargs$control <- control
+  method <- estimator.args$method
+  if (is.null(method)) {
+    cli::cli_alert_warning("No estimation method specified. Setting `method = 'hyper'.")
+    method <- "hyper"
+  }
+  valdata <- estimator.args$valdata
+  iterations_subset <- estimator.args$iterations_subset
+  ncores <- estimator.args$ncores
+  if ("verbose" %in% names(lavargs)) verbose <- FALSE
+  else verbose <- lavargs$verbose
+
+  # Initialise {lavaan} model object -------------------------------------------
+  lavargs$model <- model
+  lavargs$data <- data
+  lavargs$do.fit <- FALSE
+  fit0 <- do.call(get("cfa", envir = asNamespace("lavaan")), lavargs)
+
+  # Fit plFA -------------------------------------------------------------------
+  D <- as.matrix(DAT) - 1
+
+  # Get A matrix
+  FREE <- lavaan::inspect(fit0, what = "free")
+  lambda <- FREE$lambda  # Loading matrix
+  p <- nrow(lambda)
+  q <- ncol(lambda)
+  if (isTRUE(lavargs$std.lv)) {
+    A <- build_constrMat(P = p, Q = q, STRUCT = "simple")
+  } else {
+    cli::cli_abort("Not implemented yet.")
+  }
+
+  # CORRFLAG
+  psi <- FREE$psi
+  LTpsi <- psi[lower.tri(psi)]
+  if (length(LTpsi) > 0 & any(LTpsi > 0)) {
+    corrflag <- 1
+  } else {
+    corrflag <- 0
+  }
+
+  fit1 <- fit_plFA(
+    DATA = D,
+    CONSTR_LIST = list(CONSTRMAT = A, CORRFLAG = corrflag),
+    VALDATA = valdata,
+    METHOD = method,
+    CONTROL = control,
+    INIT = start,
+    ITERATIONS_SUBSET = iterations_subset,
+    VERBOSEFLAG = as.numeric(verbose),
+    NCORES = ncores
+  )
+  return(fit1)
+
+  # out <- create_lav_from_fitsem(fit, model, data, ...)
+  # new("brlavaan", out)
+}
---
+++ inst/lavaan_binary.R
@@ -0,0 +1,56 @@
+library(plFA)
+library(lavaan)
+set.seed(123)
+
+p <- 10  # number of items
+q <- 2  # number of latent variables
+n <- 2000  # number of observations
+
+pop_model <- "
+  # Factor loadings
+  eta1 =~ 0.8 * y1 + 0.7 * y2 + 0.47 * y3 + 0.38 * y4 + 0.34 * y5
+  eta2 =~ 0.8 * y6 + 0.7 * y7 + 0.47 * y8 + 0.38 * y9 + 0.34 * y10
+
+  # Factor correlation
+  eta1 ~~ 0.3 * eta2
+
+  # Thresholds for ordinal items
+  y1 | -1.43*t1
+  y2 | -0.55*t1
+  y3 | -0.13*t1
+  y4 | -0.72*t1
+  y5 | -1.13*t1
+  y6 | -1.43*t1
+  y7 | -0.55*t1
+  y8 | -0.13*t1
+  y9 | -0.72*t1
+  y10 | -1.13*t1
+"
+
+DAT <- simulateData(pop_model, sample.nobs = n)
+dat <- as.data.frame(lapply(DAT, ordered))
+D <- as.matrix(DAT) - 1
+mod <- "
+  eta1 =~ y1 + y2 + y3 + y4 + y5
+  eta2 =~ y6 + y7 + y8 + y9 + y10
+"
+
+tictoc::tic("DWLS Estimation")
+fit_lav1 <- cfa(mod, dat, std.lv = TRUE)
+coef(fit_lav1)
+tictoc::toc()
+tictoc::tic("Pairwise ML Estimation")
+fit_lav2 <- cfa(mod, dat, std.lv = TRUE, estimator = "PML")
+coef(fit_lav2)
+tictoc::toc()
+
+A <- build_constrMat(P = p, Q = q, STRUCT = "simple")
+fit_plFA <- fit_plFA(
+  DATA = D,
+  CONSTR_LIST = list("CONSTRMAT" = A, "CORRFLAG" = 1),
+  METHOD = "hyper"
+)
+
+# These two error
+getPar(fit_plFA, OPTION = "raw")
+computeVar(OBJ = fit_plFA, DATA = D, NUMDERIV = TRUE)
