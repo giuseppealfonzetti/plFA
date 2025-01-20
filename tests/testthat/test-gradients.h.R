5d2cdaa6ed92c70ac2d79eaef3c95516314afda2
Giuseppe Alfonzetti <giuseppe.alfonzetti@uniud.it>
2025-01-20 11:54:41 +0100

    drop old gradients tests and add seeds rng

--- tests/testthat/test-gradients.h.R
+++ tests/testthat/test-gradients.h.R
@@ -4,8 +4,6 @@
 # exportedFuns.h                                  #
 ###################################################
 
-set.seed(123)
-
 # p = number of items, q = number of latent variables, n = number of observations
 p <- 12; q <- 3; n <- 100
 
@@ -34,7 +32,6 @@ pair_nll <- function(PAR, K, L, OPTION=0){
   out <- pair$nll/n
   return(out)
 }
-
 pair_ngr <- function(PAR, K, L, OPTION=0){
   pair <- cpp_compute_pair_ext(
     CONSTRMAT   = constr_list$CONSTRMAT,
@@ -57,52 +54,10 @@ pair_ngr <- function(PAR, K, L, OPTION=0){
   return(out)
 }
 
-pair_nll_old <- function(PAR, K, L){
-  pair <- cpp_compute_pair(
-    CONSTRMAT   = constr_list$CONSTRMAT,
-    CONSTRLOGSD = constr_list$CONSTRLOGSD,
-    C_VEC       = cat,
-    THETA       = PAR,
-    CORRFLAG    = corrflag,
-    NTHR        = nthr,
-    NLOAD       = nload,
-    NCORR       = ncorr,
-    NVAR        = nvar,
-    K           = K,
-    L           = L,
-    PAIRS_TABLE = f,
-    SILENTFLAG  = 1,
-    GRADFLAG    = 0,
-    OPTION      = 0
-  )
-  out <- pair$ll/n
-  return(out)
-}
-pair_ngr_old <- function(PAR, K, L){
-  pair <- cpp_compute_pair(
-    CONSTRMAT   = constr_list$CONSTRMAT,
-    CONSTRLOGSD = constr_list$CONSTRLOGSD,
-    C_VEC       = cat,
-    THETA       = PAR,
-    CORRFLAG    = corrflag,
-    NTHR        = nthr,
-    NLOAD       = nload,
-    NCORR       = ncorr,
-    NVAR        = nvar,
-    K           = K,
-    L           = L,
-    PAIRS_TABLE = f,
-    SILENTFLAG  = 1,
-    GRADFLAG    = 1,
-    OPTION      = 0
-  )
-  out <- pair$ngr/n
-  return(out)
-}
-
 #### (STDLV=FALSE, CORRFLAG=TRUE) ####
 #### free correlation matrix and latent variances ######
 {
+  set.seed(123)
   stdlv <- FALSE
   corrflag <- TRUE
 
@@ -176,6 +131,7 @@ pair_ngr_old <- function(PAR, K, L){
 #### (STDLV=TRUE, CORRFLAG=TRUE) ####
 #### free correlation matrix but all latent variances fixed ######
 {
+  set.seed(123)
   stdlv <- TRUE
   corrflag <- TRUE
 
@@ -249,6 +205,7 @@ pair_ngr_old <- function(PAR, K, L){
 #### free correlation matrix but all latent variances fixed ######
 #### some variances specified by the user ####
 {
+  set.seed(123)
   stdlv <- TRUE
   corrflag <- TRUE
 
@@ -318,80 +275,10 @@ pair_ngr_old <- function(PAR, K, L){
   }
 }
 
-
-{
-  stdlv <- TRUE
-  corrflag <- TRUE
-
-  # Simple loading matrix constraints
-  A <- build_constrMat(P = p, Q = q, STRUCT = 'simple')
-  A <- check_cnstr_loadings(A, stdlv)
-
-  # Draw some random loadings according to constraints
-  Load <- gen_loadings(CONSTRMAT = A, STDLV = stdlv)
-  nload <- sum(is.na(A))
-
-  # Generate random latent correlation matrix
-  tcorrvec <- rep(0, q*(q-1)/2); if(corrflag) tcorrvec <- rnorm(q*(q-1)/2)
-  ncorr <- if(corrflag)q*(q-1)/2 else 0
-  R <- cpp_latvar_vec2cmat(VEC=tcorrvec, NCORR=ncorr, Q=q)
-
-
-  # Generate random latent variances
-  constr_var <- rep(NA, q); constr_var[2] <- .5
-  constr_lsd <- check_cnstr_latvar(constr_var, q, stdlv)
-  nvar  <- sum(is.na(constr_lsd))
-  tsdvec <- constr_lsd
-  tsdvec[is.na(constr_lsd)] <- rnorm(sum(is.na(constr_lsd)), -1, .1)
-  Dmat <- diag(exp(tsdvec),q,q)
-
-  S <- Dmat %*% R %*% Dmat
-
-  # Dimensions
-  constr_list <- check_cnstr(list(CONSTRMAT=A, CORRFLAG=corrflag, CONSTRVAR=exp(constr_lsd)^2, STDLV=stdlv))
-  d <- nthr + nload + ncorr + nvar
-  d
-
-
-  #
-  theta <- get_theta(
-    THRESHOLDS = rep(thr, p),
-    LOADINGS = Load,
-    LATENT_COV = S,
-    CAT = cat,
-    CONSTRMAT = A,
-    CONSTRVAR = exp(constr_lsd)^2,
-    CORRFLAG = corrflag,
-    STDLV = stdlv
-  )
-
-  dat <- sim_data(
-    SAMPLE_SIZE = n,
-    LOADINGS = Load,
-    THRESHOLDS = thr,
-    LATENT_COV = S)
-  f <- compute_frequencies(Y = dat, C_VEC = cat)
-
-  dat <- check_data(dat)
-  constr_list <- check_cnstr(list(CONSTRMAT=A, CORRFLAG=corrflag, CONSTRVAR=exp(constr_lsd)^2, STDLV=stdlv))
-  dims <- check_dims(dat, constr_list)
-
-  for (l in 2:p) {
-    for (k in 1:(l-1)) {
-      test_that(paste0("check gradient pair (", l, ",", k, "):") ,{
-        expect_equal(pair_ngr(PAR=theta, K=k, L=l), numDeriv::grad(func=pair_nll, x=theta, K=k,L=l), tolerance = 1e-4)
-      })
-
-      test_that(paste0("check gradient (pi version) pair (", l, ",", k, "):") ,{
-        expect_equal(pair_ngr(PAR=theta, K=k, L=l, OPTION=1), numDeriv::grad(func=pair_nll, x=theta, K=k,L=l), tolerance = 1e-4)
-      })
-    }
-  }
-}
-
 #### (STDLV=FALSE, CORRFLAG=FALSE) ####
 #### no correlation matrix but some free latent variances ######
 {
+  set.seed(123)
   stdlv <- FALSE
   corrflag <- FALSE
 
@@ -464,6 +351,7 @@ pair_ngr_old <- function(PAR, K, L){
 #### (STDLV=FALSE, CORRFLAG=TRUE) ####
 #### no correlation matrix but all free latent variances ######
 {
+  set.seed(123)
   stdlv <- FALSE
   corrflag <- FALSE
 
