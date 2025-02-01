012da771d3a88a23f53a0742eb4195efebb166a5
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2025-02-01 12:45:05 +0800

    Turn of test coverage for now

--- tests/testthat/test-fit_plFA.R
+++ tests/testthat/test-fit_plFA.R
@@ -1,6 +1,6 @@
-# if (.Platform$OS.type == "windows") {
-#   testthat::skip("Skipping this file on Windows because of time constraints")
-# }
+if (.Platform$OS.type == "windows") {
+  testthat::skip()
+}
 
 # p = number of items, q = number of latent variables, n = number of observations
 p <- 30; q <- 8; n <- 1000
