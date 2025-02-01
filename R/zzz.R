d908f32c93033884dfd6dded34f2fbe6ce059c4c
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2025-02-01 11:37:45 +0800

    Hack startup messages inspired by tidyverse (fix #4)

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,30 @@
 .onAttach <- function(libname, pkgname) {
 
   # Load lavaan
-  # cli::cli_alert_info(
-  #   "Loading required package: {.pkg lavaan}",
-  #   class = "packageStartupMessage"
-  # )
-  # library("lavaan", exclude = "cfa")
+  startup_msg <- paste0(
+    cli::cli_fmt(cli::cli_alert_info("Loading required package: {.pkg lavaan}"))
+  )
+  rlang::inform(startup_msg, class = "packageStartupMessage")
+  attach_lavaan(without = "cfa")
 
   # Inform conflicts
-  # conflict_msg <- paste0(
-  #   cli::rule(left = cli::style_bold("Conflicts")),
-  #   "\n",
-  #   bullets_mask("plFA", "lavaan", "cfa")
-  # )
-  # rlang::inform(conflict_msg, class = "packageStartupMessage")
+  conflict_msg <- paste0(
+    cli::rule(
+      left = cli::style_bold("Conflicts"),
+      right = paste("plFA", utils::packageVersion("plFA"))
+    ),
+    "\n",
+    bullets_mask("plFA", "lavaan", "cfa")
+  )
+  rlang::inform(conflict_msg, class = "packageStartupMessage")
+}
+
+attach_lavaan <- function(without) {
+  do.call("library", list(
+    package = "lavaan",
+    exclude = without,
+    character.only = TRUE
+  ))
 }
 
 format_pkgfun <- function(pkg, fun) {
