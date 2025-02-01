b4e0a2ba5f2e7a42b21e871639b7be13605a2154
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2025-02-01 10:53:53 +0800

    Revert back, will not pass CRAN checks (issue #4)

--- R/zzz.R
+++ R/zzz.R
@@ -1,19 +1,19 @@
 .onAttach <- function(libname, pkgname) {
 
   # Load lavaan
-  cli::cli_alert_info(
-    "Loading required package: {.pkg lavaan}",
-    class = "packageStartupMessage"
-  )
-  library("lavaan", exclude = "cfa")
+  # cli::cli_alert_info(
+  #   "Loading required package: {.pkg lavaan}",
+  #   class = "packageStartupMessage"
+  # )
+  # library("lavaan", exclude = "cfa")
 
   # Inform conflicts
-  conflict_msg <- paste0(
-    cli::rule(left = cli::style_bold("Conflicts")),
-    "\n",
-    bullets_mask("plFA", "lavaan", "cfa")
-  )
-  rlang::inform(conflict_msg, class = "packageStartupMessage")
+  # conflict_msg <- paste0(
+  #   cli::rule(left = cli::style_bold("Conflicts")),
+  #   "\n",
+  #   bullets_mask("plFA", "lavaan", "cfa")
+  # )
+  # rlang::inform(conflict_msg, class = "packageStartupMessage")
 }
 
 format_pkgfun <- function(pkg, fun) {
