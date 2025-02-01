72c179c4e9bb414ed62aa0e53870aab08d8001d6
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2025-02-01 17:22:36 +0800

    Update docs

--- R/data.R
+++ R/data.R
@@ -1,9 +1,17 @@
 #' @inherit ltm::LSAT title description format source references
+#' @examples
+#' library(tidyverse)
+#' glimpse(LSAT)
+#'
 "LSAT"
 
-#' @inherit psych::bfi title description format source references
+#' @inherit psych::bfi title description format note source references
 #'
 #' @details Unlike the `psych::bfi` data sets, the `bfi` data set in this
 #' package retains only complete cases of all variables.
+#' @seealso The [`psych::bfi`] data set from the `{psych}` package.
+#' @examples
+#' library(tidyverse)
+#' glimpse(bfi)
 #'
 "bfi"
