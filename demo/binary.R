9d4bbccf26265df9bd9ff0a8be7d38f815f241e7
Haziq Jamil <haziq.jamil@ubd.edu.bn>
2025-02-12 16:18:39 +0800

    Update binary.R

--- demo/binary.R
+++ demo/binary.R
@@ -2,7 +2,8 @@ library(plFA)
 data("LSAT")
 mod <- "eta =~ y1 + y2 + y3 + y4 + y5"
 fit <- cfa(mod, LSAT, std.lv = TRUE, estimator = "PML", se = "none")
+summary(fit)
 
-summary(update(fit, se = "robust.huber.white"))
-summary(update(fit, se = TRUE))
-summary(update(fit, se = "robust.huber.white", test = "mean.var.adjusted"))
+# summary(update(fit, se = "robust.huber.white"))
+# summary(update(fit, se = "robust.huber.white", test = "mean.var.adjusted"))
+# summary(update(fit, se = TRUE))
